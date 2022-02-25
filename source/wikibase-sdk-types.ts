887e034376e51237928c68d32ff3e55bd1aeec5d
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-02-25 20:10:47 +0100

    refactor(xo): adapt to new rules

--- source/wikibase-sdk-types.ts
+++ source/wikibase-sdk-types.ts
@@ -4,9 +4,9 @@ export interface EntitySimplified {
 	readonly type: string;
 	readonly id: string;
 	readonly modified?: string;
-	readonly aliases?: Record<string, readonly string[]>;
-	readonly claims?: Record<string, readonly ClaimSimplified[]>;
-	readonly descriptions?: Record<string, string>;
-	readonly labels?: Record<string, string>;
-	readonly sitelinks?: Record<string, string>;
+	readonly aliases?: Readonly<Record<string, readonly string[]>>;
+	readonly claims?: Readonly<Record<string, readonly ClaimSimplified[]>>;
+	readonly descriptions?: Readonly<Record<string, string>>;
+	readonly labels?: Readonly<Record<string, string>>;
+	readonly sitelinks?: Readonly<Record<string, string>>;
 }
